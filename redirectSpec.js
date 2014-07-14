ca380d49090f255e859d2cd39ece5bab3feec529
Joe Helfrich <joe.helfrich@gaiam.com>
2014-07-14 15:43:16 -0600

    fixed scope issue

--- redirectSpec.js
+++ redirectSpec.js
@@ -3,19 +3,19 @@
  * be requested and the URL to be redirected to seperated by a space.
  */
 
-var url = "http://myotest-qa.gaiam.com",
-    testPair = [],
+var url = "www.gaiam.com",
     http = require('http'),
     fs = require('fs');
 
 describe('The URL at', function() {
   fs.readFileSync('./redirects.txt').toString().split('\n').forEach(function (line) {
-    testPair = line.split(' ');
+    var testPair = line.split(' ');
     it(testPair[0] + ' should redirect to ' + testPair[1], function(done){
-      var response = http.get(url + testPair[0]);
-      expect(response.statusCode).toEqual(301);
-      expect(response.headers.location).toEqual(testPair[1]);
-      done();
+      http.get(url + testPair[0], function(response) {
+        expect(response.statusCode).toEqual(301);
+        expect(response.headers.location).toEqual(testPair[1]);
+        done();
+      });
     });
   });
 });
