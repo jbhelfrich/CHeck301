81e03d8fb45ca5f12e10b86ce9e622820cf549c7
Joe Helfrich <joe.helfrich@gaiam.com>
2014-07-15 10:01:53 -0600

    Working spec file for Jasmine Node included. Better output formatting.

--- redirectSpec.js
+++ redirectSpec.js
@@ -3,19 +3,25 @@
  * be requested and the URL to be redirected to seperated by a space.
  */
 
-var url = "http://www.gaiam.com",
+// Some systems seem to require that url includes the site protocol (http://)
+// and some complain if it is present.  For example, Ubuntu seems to need it
+// but ArchLinux does not.  If you get errors retriving a site, try adding
+// or removing that first.
+var url = "http://myotest-qa.gaiam.com",
     http = require('http'),
     fs = require('fs');
 
 describe('The URL at', function() {
   fs.readFileSync('./redirects.txt').toString().split('\n').forEach(function (line) {
-    var testPair = line.split(' ');
-    it(testPair[0] + ' should redirect to ' + testPair[1], function(done){
-      http.get(url + testPair[0], function(response) {
-        expect(response.statusCode).toEqual(301);
-        expect(response.headers.location).toEqual(testPair[1]);
-        done();
+    if (line.length > 0){
+      var testPair = line.split(' ');
+      it(testPair[0] + ' should redirect to ' + testPair[1], function(done){
+        http.get(url + testPair[0], function(response) {
+          expect(response.statusCode).toEqual(301);
+          expect(response.headers.location).toEqual(testPair[1]);
+          done();
+        });
       });
-    });
+    }
   });
 });
--- redirectTest.js
+++ redirectTest.js
@@ -8,27 +8,47 @@ var url = "http://myotest-qa.gaiam.com",
     http = require('http'),
     lineReader = require('line-reader');
 
-describe('Placeholder', function() {
-  it('should pass', function(done){
-    expect(true).toBeTruthy;
+function check301(source, destination){
+  var options = {
+    hostname: 'myotest-qa.gaiam.com',
+    port: 80,
+    path: source,
+    method: 'GET',
+}
+  var request = http.request(options, function(response){
+    if (response.statusCode != 301 || 
+        response.headers.location != destination){
+      console.log(source + ' does not redirect to ' + destination);
+    } else {
+      //console.log(source + ' passed!');
+    }
+  }).on('error', function(e){
+    console.log(e.message);
+    console.log(e.stack);
   });
+  request.end();
+}
+
+/* 
+//Unthrottled version. Use with caution.
+lineReader.eachLine('redirects.txt', function(line){
+  testPair = line.split(' ');
+  check301(testPair[0], testPair[1]);
+  console.log(line);
 });
 
-describe('The URL at', function() {
-  console.log('Entering Describe');
-  function check301(source, destination){
-    console.log('Entering check');
-    it(source + ' should redirect to ' + destination, function(done){
-      var response = http.get(url + source);
-      expect(response.statusCode).toEqual(301);
-      expect(response.headers.location).toEqual(destination);
-      done();
-    });
-  });
-  
-  lineReader.eachLine('redirects.txt', function(line){
-    console.log('Processing ' + line);
-    testPair = line.split(' ');
-    check301(testPair[0], testPair[1]);
-  });
+*/
+//Throttled version. 
+lineReader.open('redirects.txt', function(reader){
+  var interval = setInterval(function(){
+    if(reader.hasNextLine()){
+      reader.nextLine(function(line){
+        testPair = line.split(' ');
+        check301(testPair[0], testPair[1]);
+      });
+    } else {
+      clearInterval(interval);
+      console.log('Done');
+    }
+  }, 200);
 });
